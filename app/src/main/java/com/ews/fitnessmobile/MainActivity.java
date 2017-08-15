bd3b5ce7eac9b503e36289c087514dd5572ac89a
Eduardo Wallace Da Silva <eduardo.silva@ntk.com.br>
2017-08-15 01:30:58 -0300

    - Adicionando SearchView

--- app/src/main/java/com/ews/fitnessmobile/MainActivity.java
+++ app/src/main/java/com/ews/fitnessmobile/MainActivity.java
@@ -1,6 +1,5 @@
 package com.ews.fitnessmobile;
 
-import android.*;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -118,6 +117,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -136,9 +136,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/ews/fitnessmobile/fragments/UnitsFragment.java
+++ app/src/main/java/com/ews/fitnessmobile/fragments/UnitsFragment.java
@@ -8,9 +8,13 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 import com.ews.fitnessmobile.MainActivity;
 import com.ews.fitnessmobile.R;
@@ -21,9 +25,10 @@ import com.ews.fitnessmobile.dao.UnidadeDAO;
 import com.ews.fitnessmobile.model.Unidade;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class UnitsFragment extends Fragment {
+public class UnitsFragment extends Fragment implements SearchView.OnQueryTextListener, MenuItem.OnActionExpandListener {
 
     private static final String TAG_LOG = "[UnitsFragment]";
     public static final String PUT_UNIT = "UNIT";
@@ -36,13 +41,14 @@ public class UnitsFragment extends Fragment {
     private Context ctx;
     private ImageView imgViewOptions;
 
+    private List<Unidade> unidades;
+
 
     public UnitsFragment() {
         // Required empty public constructor
         this.fitnessAPI = APIUtils.getFitnessAPI();
         MainActivity.fabAdd.setVisibility(FloatingActionButton.VISIBLE);
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,19 +67,66 @@ public class UnitsFragment extends Fragment {
                 new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL);
         this.recyclerView.addItemDecoration(itemDecoration);
 
-        this.unitsAdapter = new UnitsAdapter(new ArrayList<Unidade>(), view.getContext(), getActivity());
+        this.unidadeDAO = new UnidadeDAO(view.getContext());
+        this.unidades = this.unidadeDAO.getAll();
+
+        this.unitsAdapter = new UnitsAdapter(this.unidades, view.getContext(), getActivity());
         this.recyclerView.setAdapter(this.unitsAdapter);
 
-        this.unidadeDAO = new UnidadeDAO(view.getContext());
-
-        loadUnits();
+        setHasOptionsMenu(true);
 
         return view;
     }
 
-    private void loadUnits() {
-        unitsAdapter.update(this.unidadeDAO.getAll());
+    public void resetSearch() {
+        unitsAdapter.update(this.unidades);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+        searchView.setQueryHint("Search"); //TODO alterar para buscar so string com suporte aos idiomas portugues e ingles
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (newText == null || newText.trim().isEmpty()) {
+            resetSearch();
+            return false;
+        }
+
+        List<Unidade> filteredValues = new ArrayList<Unidade>(this.unidades);
+        for (Unidade value : this.unidades) {
+
+            if (!value.getCidade().toLowerCase().contains(newText.toLowerCase())) {
+                filteredValues.remove(value);
+            }
+
+        }
+
+        unitsAdapter.update(filteredValues);
+
+        return false;
+    }
+
+    @Override
+    public boolean onMenuItemActionExpand(MenuItem item) {
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemActionCollapse(MenuItem item) {
+        return true;
+    }
 
 }
